867babdef925ea73ca160b3f999734e81057da5e
Shizzyq <44840391+Shizzyq@users.noreply.github.com>
2019-05-15 20:49:44 +0200

    Add files via upload

---
+++ just.js
@@ -0,0 +1,22 @@
+function showDiv(){
+    document.querySelector('#info').style.visibility = 'visible';
+}
+
+function getInfo() {
+    let randomNumber = Math.floor((Math.random() * 88) + 1)
+    let apiUrl = 'https://swapi.co/api/people/' + randomNumber
+
+    axios.get(apiUrl).then(function(response){
+        updateInfo(response.data)
+    });
+}
+
+function updateInfo(data){
+    document.getElementById('name').innerText = 'name: ' + data.name;
+    document.getElementById('height').innerText = 'height: ' + data.height;
+    document.getElementById('mass').innerText = 'mass: ' + data.mass;
+    document.getElementById('birth-year').innerText = 'birth: ' + data.birth_year;
+}
+
+
+button.addEventListener('click', getInfo);
